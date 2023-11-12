8b7275c65b3d521ad32da2aababb1e273db91e4e
enis <enis_akyol@yahoo.com>
2023-11-12 12:44:47 +0300

    Data SQL Queries

---
+++ src/main/java/com/cydeo/SpringDataQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.springdataqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataQueriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataQueriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/springdataqueries/SpringDataQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.springdataqueries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataQueriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
